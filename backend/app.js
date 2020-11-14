e947974b3a392ed937fb6c31906c366d5c4b7b79
Tiyari Harshita <e0316032@u.nus.edu>
2020-11-14 22:36:33 +0800

    add db

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,24 @@ const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
+const CONNECTION_URL = "mongodb+srv://har:123456SOS@quotes.3lnlm.mongodb.net/Quotes?retryWrites=true&w=majority";
+const DATABASE_NAME = "Quotes";
+
 var app = Express();
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {});
+var database, collection;
+
+app.listen(3000, () => {
+    MongoClient.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("quotes");
+        console.log(collection)
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
