38ffea3cddca1bf7afd7e9ee0aa106d3c29ee780
Tiyari Harshita <e0316032@u.nus.edu>
2020-11-14 22:09:41 +0800

    add app.js

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {});
